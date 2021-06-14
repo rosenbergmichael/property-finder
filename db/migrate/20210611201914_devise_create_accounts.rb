07d26386ee67d0716222573790c93d53e20bbb58
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-06-14 10:25:41 -0400

    in devise_create_accounts.rb migration, in Database authenticatable section, add strings for first_name, last_name and url

--- db/migrate/20210611201914_devise_create_accounts.rb
+++ db/migrate/20210611201914_devise_create_accounts.rb
@@ -4,8 +4,11 @@ class DeviseCreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts do |t|
       ## Database authenticatable
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :url
 
       ## Recoverable
       t.string   :reset_password_token
