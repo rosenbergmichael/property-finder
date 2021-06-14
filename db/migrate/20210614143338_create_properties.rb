e243667a45f9cf1b1eecc4136fe8d561ae29dc3d
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-06-14 10:44:27 -0400

    rollback db, in properties table add account references, migrate db

--- db/migrate/20210614143338_create_properties.rb
+++ db/migrate/20210614143338_create_properties.rb
@@ -1,6 +1,7 @@
 class CreateProperties < ActiveRecord::Migration[6.0]
   def change
     create_table :properties do |t|
+      t.references :account 
       t.string :name
       t.string :address
       t.integer :price
--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_143338) do
   end
 
   create_table "properties", force: :cascade do |t|
+    t.integer "account_id"
     t.string "name"
     t.string "address"
     t.integer "price"
@@ -45,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_143338) do
     t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
 end
