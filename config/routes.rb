bb5ebd991bc63a3c8b8c0c48f2f32bcf5fbd2346
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-06-21 17:55:49 -0400

    in config routes.rb set custom route for dashboard index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
+  get 'dashboard#index', as: :dashboard
   get 'dashboard/properties'
   get 'dashboard/reports'
+
   resources :properties
   devise_for :accounts
   root to: 'public#main'
